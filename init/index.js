34306fe42855f18d44b7bf9b605a7d6dfeeaf407
armaan941 <armaanmulani941@gmail.com>
2026-03-24 17:35:00 +0530

    Initial commit - Wanderlust full stack project

---
+++ init/index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then((res) => {
+    console.log("Succesfully Connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "69be711e1a98292aa3155f82",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("Data was initialized");
+};
+
+initDB();
